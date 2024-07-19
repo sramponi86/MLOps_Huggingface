9effb3343cd6b978cf10a89a7f809ee9dc6eedd1
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2024-07-19 11:32:53 +0000

    fixes

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-with gr.Blocks() as demo:
-    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs="text")
+with gradio.Blocks() as demo:
+    textbox = gradio.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gradio.Interface(fn=predict, inputs=textbox, outputs="text")
 
 demo.lauch()
